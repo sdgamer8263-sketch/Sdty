7f3a491edffef79ce39746534329982eef6800e7
Sourin De <sdgamer8263@gmail.com>
2026-05-19 00:14:03 +0530

    feat: Add end-to-end test for registration
    Introduces a basic Puppeteer test to verify the registration page functionality.
    This helps ensure critical user flows are working as expected.

--- firebase.js
+++ firebase.js
@@ -1,8 +1,15 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-import firebaseConfig from './firebase-applet-config.json';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBvlOV7L3g_lBDRR7mcNincGVyM16xtd90",
+  authDomain: "ska-web-168fe.firebaseapp.com",
+  databaseURL: "https://ska-web-168fe-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "ska-web-168fe",
+  appId: "1:782823377176:web:e2f62ba16d5fe0af7f366d"
+};
 
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app, firebaseConfig.firestoreDatabaseId);
+export const db = getFirestore(app);
 export const auth = getAuth(app);
